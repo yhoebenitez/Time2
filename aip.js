c9a2c29fd6d07851de7802004f8f90a7faf95ca8
Airam Ponce <eliu.ponce@gmail.com>
2018-07-31 12:17:10 -0400

    First commit Airam Ponce

---
+++ aip.js
@@ -0,0 +1,37 @@
+/******************************
+* TIME
+* The Time
+* (El tiempo - Primer Proyecto asignado)
+* Airam Ponce
+*/
+/// VARIABLES
+    // get a number (pasar el numero)
+    const MesesEn = [ 'January', 'February', 'March',
+    'April', 'May', 'June', 'July',
+    'August', 'September', 'October', 
+    'November', 'December',];
+    const MesesEsp = [ 'Enero', 'Febrero', 'Marzo',
+    'Abril', 'Mayo', 'Junio', 'Julio',
+    'Agosto', 'Septiembre', 'Octubre', 
+    'Noviembre', 'Diciembre',];
+    const MesesFr = [ 'Janvier', 'Février', 'Mars',
+    'Avril', 'Mai', 'Juni', 'Juillet',
+    'Aout', 'Septembre', 'Octobre', 
+    'Novembre', 'Décembre',];
+    
+    // FUNCTIONS
+    
+    function MesesDias( MesesEn ) {
+    console.log('Numero convertido en el mes de ' + 
+    MesesEn)
+    
+    }
+    
+    
+    // RUN/
+    
+    MesesDias(MesesEn[8]);
+    MesesDias(MesesEsp[7]);
+    MesesDias(MesesFr[7]);
+    
+    
---
+++ index.js
@@ -0,0 +1,12 @@
+/******************************
+* TIME
+* The Time
+* (El tiempo - Primer Proyecto asignado)
+* Airam Ponce / Franklin Rodriguez / Ariday Morales / Yhoe Benitez
+*/
+
+/// VARIABLES
+   
+// FUNCTIONS
+
+// RUN/
